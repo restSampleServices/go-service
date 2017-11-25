9f9f5e665e902ed9f4705fa1fc3677e837511a2d
blndev <blndev@users.noreply.github.com>
2017-11-25 22:13:27 +0100

    adding some notes related to build info

--- BuildInfo/BuildInfo.go
+++ BuildInfo/BuildInfo.go
@@ -1,5 +1,11 @@
 package BuildInfo
 
-var BuildTime = "12345"
+//All of these Information will be injected via compiler flasgs.
+//Changes here will have no effects.
+
+//The Time where the build was made
+var BuildTime = ""
+//The Commit where thebuild is based on.
 var Commit = "n.a."
+//The Version of the Service
 var Version = "unknown"
